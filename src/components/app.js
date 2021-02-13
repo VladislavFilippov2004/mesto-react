b2e4eaf47efb243aef7687c034401f3866b40908
VladislavFilippov2004 <vvf0807@gmail.com>
2021-02-13 15:22:20 +0300

    Проектная работа 10

--- src/components/app.js
+++ src/components/app.js
@@ -7,7 +7,7 @@ import '../index.css';
 import PopupWithForm from './popupWithForm.js'
 import ImagePopup from './imagePopup.js'
 
-function App(props) {
+function App() {
     const [isEditProfilePopupOpen, setEditProfilePopup] = React.useState(false);
     function handleEditProfileClick() {
         setEditProfilePopup(true)
@@ -26,7 +26,6 @@ function App(props) {
     const [selectedCard, setSelectedCard] = React.useState(null)
     const [isOpen, setIsOpen] = React.useState(false)
     function handleCardClick(card) {
-        console.log('ll')
         setIsOpen(true);
         setSelectedCard(card)
     }
@@ -51,36 +50,27 @@ function App(props) {
             onCardClick={handleCardClick}>
             </Main>
 
-            <PopupWithForm name="new-avatar" title="Обновить аватар"  isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
+            <PopupWithForm name="new-avatar" title="Обновить аватар" buttonText="Сохранить"  isOpen={isEditAvatarPopupOpen} onClose={closeAllPopups}>
                 <fieldset className="popup__info">
                     <input type="url" className="popup__input popup__input_link" id="link" name='link'
                         placeholder="Ссылка на картинку" required />
                     <span className='span-error link-error popup__span-error'></span>
                 </fieldset>
-                <button type="submit" className="popup__button-save popup__button-save_form" name="make-form-btn">
-                    Сохранить
-                </button>
             </PopupWithForm>
 
-            <PopupWithForm name="confrim" title="Вы уверены?" >
-                <button type="submit" className="popup__button-save" name="submit-form-btn">
-                    Да
-                </button>
+            <PopupWithForm name="confrim" title="Вы уверены?" buttonText="Да">
             </PopupWithForm>
 
-            <PopupWithForm name="new-place" title="Новое место" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
+            <PopupWithForm name="new-place" title="Новое место" buttonText="Создать" isOpen={isAddPlacePopupOpen} onClose={closeAllPopups}>
                 <fieldset className="popup__info">
                     <input type="text" className="popup__input popup__input_place " id="place-input" name='name' placeholder="Название места" minLength="2" required />
                     <span className='span-error place-input-error popup__span-error'></span>
                     <input type="url" className="popup__input popup__input_link" id="link-input" name='link' placeholder="Ссылка на картинку" required />
                     <span className='span-error link-input-error popup__span-error'></span>
                 </fieldset>
-                <button type="submit" className="popup__button-save popup__button-save_form" name="make-form-btn">
-                    Создать
-                </button>
             </PopupWithForm>
 
-            <PopupWithForm name="user-info" title="Редактировать профиль" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
+            <PopupWithForm name="user-info" title="Редактировать профиль" buttonText="Сохранить" isOpen={isEditProfilePopupOpen} onClose={closeAllPopups}>
                 <fieldset className="popup__info">
 
                     <input type="text" className="popup__input popup__input_name" id="name-input" name="input-user-name" minLength="2" required />
@@ -89,10 +79,6 @@ function App(props) {
                     <span className='span-error job-input-error popup__span-error'></span>
 
                 </fieldset>
-                
-                <button type="submit" className="popup__button-save" name="submit-form-btn" >
-                    Сохранить
-                </button>
             </PopupWithForm>
             <ImagePopup card={selectedCard} isOpen={isOpen} onClose={closeAllPopups}/>      
             <Footer></Footer>
--- src/components/popupWithForm.js
+++ src/components/popupWithForm.js
@@ -7,6 +7,9 @@ function PopupWithForm (props) {
             <form className="popup__form popup__form_user-info" method="GET" name={props.name}>
             <h3 className="popup__title">{props.title}</h3>
             {props.children}
+            <button type="submit" className="popup__button-save popup__button-save_form" name="make-form-btn">
+                    {props.buttonText}
+                </button>
             <button className="popup__icon-close" type="reset" onClick={props.onClose}>
                     <img src={closeIcon} alt="Кнопка закрыть" />
                 </button>
